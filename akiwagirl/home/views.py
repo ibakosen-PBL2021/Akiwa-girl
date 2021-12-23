a1ebb94e418a4774a42d9582ef926dc136f5afcc
Roma Tsutsumi <st18140rm@gm.ibaraki-ct.ac.jp>
2021-12-23 11:58:04 +0900

    make home refer to the model

--- akiwagirl/home/views.py
+++ akiwagirl/home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from num.models import Current
 def index(request):
-    num = 12
-    crowdedness = "快適"
-    if num > 10:
+    num = Current.objects.get().num
+    if num > 20:
+        crowdedness = "混雑"
+    elif num > 15:
+        crowdedness = "やや混雑"
+    elif num > 10:
         crowdedness = "やや快適"
+    else:
+        crowdedness = "快適"
     dist = {
         'num': num,
         'crowdedness': crowdedness,
--- akiwagirl/num/views.py
+++ akiwagirl/num/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from .models import Current
 # Create your views here.
 def index(request):
-    num = Current.objects.get()
-    return render(request, 'num/index.html', {'num':num})
+    num = Current.objects.get().num
+    if num > 20:
+        crowdedness = "混雑"
+    elif num > 15:
+        crowdedness = "やや混雑"
+    elif num > 10:
+        crowdedness = "やや快適"
+    else:
+        crowdedness = "快適"
+    dist = {
+        'num': num,
+        'crowdedness': crowdedness,
+    }
+    return render(request, 'num/index.html', dist)
